40331c0045cc93957a241aa67b9a2016b80d9521
Wellington Victor <81436312+victorwellington02@users.noreply.github.com>
2021-05-17 08:05:07 -0300

    Desafio Finalizado

---
+++ index.js
@@ -0,0 +1,10 @@
+const nome = "Wellington";
+const idade = 18;
+var dataAtual = new Date(2021);
+const humano = true;
+var n1 = 10;
+var n2 = 2;
+
+console.log(`Olá, meu nome é ${nome}, e meu ano de nascimento é ${(dataAtual - idade)}.`)
+console.log(humano)
+console.log(n1+n2)
